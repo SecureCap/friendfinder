c9909e38a713f34b4a71fde3038e197921fd0250
Secure Cap <Twilliams397tw@gmail.com>
2018-08-30 19:11:32 -0500

    added addition symbol app.use function

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,11 +9,11 @@ module.exports = function(app) {
 //html gets requests
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(_dirname, "/../public/survey.html"));
+    res.sendFile(path.join(_dirname + "/../public/survey.html"));
 });
 
 //if no matching route, default to home
-app.get("/home", function(req, res) {
-    res.sendFile(path.join(_dirname, "/../public/home.html"));
+app.use(function(req, res) {
+    res.sendFile(path.join(_dirname + "/../public/home.html"));
 });
 }
