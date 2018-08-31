3ad267a1f72c780b32f12395fb18ee95bc093588
Secure Cap <Twilliams397tw@gmail.com>
2018-08-31 10:00:47 -0500

    directory route edits

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,11 +9,11 @@ module.exports = function(app) {
 //html gets requests
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(_dirname + "../public/survey.html"));
+    res.sendFile(path.join(_dirname, "../public/survey.html"));
 });
 
 //if no matching route, default to home
 app.use(function(req, res) {
-    res.sendFile(path.join(_dirname + "../public/home.html"));
+    res.sendFile(path.join(_dirname, "../public/home.html"));
 });
 }
